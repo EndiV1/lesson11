925b706f0515d28133960b4cdb9b1e0b50355d4f
EndiV1 <165320431+EndiV1@users.noreply.github.com>
2025-05-08 20:48:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ file_path = "C:/Users/Student/Desktop/lesson11/leximi.txt"
 ## line1 = file.readline()
   # print(line1)
 
+#with open(file_path, "w") as file:
+# #file.write("hello world")
+
 with open(file_path, "w") as file:
     file.write("hello world")
+    file.seek(20)
+    print(file)
+
+
+
+
